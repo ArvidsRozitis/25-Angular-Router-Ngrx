17d9ec696384744ea051bddae45e3997c6e674a3
Arvids Rozitis <arvids.rozitis.cits@gmail.com>
2023-03-05 23:57:30 +0200

    sidebar roating done.

--- recipes-app/src/pages/single-recipe-page/single-recipe-page.component.ts
+++ recipes-app/src/pages/single-recipe-page/single-recipe-page.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {recipesData, RecepiesDataInterface} from "../../assets/hardCodedRecipes"
+import { recipesData } from '../../assets/hardCodedRecipes';
 
 @Component({
   selector: 'app-single-recipe-page',
   templateUrl: './single-recipe-page.component.html',
-  styleUrls: ['./single-recipe-page.component.scss']
+  styleUrls: ['./single-recipe-page.component.scss'],
 })
 export class SingleRecipePageComponent {
-  constructor(private route:ActivatedRoute) {}
-  ngOnInit() {}
-  
-  recipeId = this.route.snapshot.paramMap.get('id')
-  recipeById = recipesData.find(obj => obj.id === this.recipeId);
-  recipe = this.recipeById === undefined ? recipesData[0] : this.recipeById
+  constructor(private activeRoute: ActivatedRoute) {}
 
+  recipeId = '';
+  recipeById = recipesData.find((obj) => obj.id === this.recipeId);
+  recipe = this.recipeById === undefined ? recipesData[0] : this.recipeById;
+
+  //here we need use null object aproach
+
+  ngOnInit() {
+    this.activeRoute.params.subscribe((routeParams) => {
+      (this.recipeId = routeParams['id']),
+        (this.recipeById = recipesData.find((obj) => obj.id === this.recipeId));
+      this.recipe =
+        this.recipeById === undefined ? recipesData[0] : this.recipeById;
+      console.log(routeParams['id']);
+    });
+  }
 }
